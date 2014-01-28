8cf1159d195b73e7026f5eb7e2618090144c665c
Hamilton Chan <hamiltonchanjr@gmail.com>
2014-01-28 10:14:29 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsOnTap::Application.config.secret_key_base = '5c1ee6e414fbe0f1e1cef6db0c1da0a783fdc741b92e86d6425bdd3cab157fc963ca40383dec0e078684045cf80e256b3ebbbdb1b37af1103ddef699a0b31ca2'
